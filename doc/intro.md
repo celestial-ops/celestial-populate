f24e4de00945966c41dd0283f9b1684403233553
ronen <narkisr@gmail.com>
2015-04-14 17:06:15 +0300

    initial import

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to celestial-populate
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject celestial-populate "0.1.0-SNAPSHOT"
+  :description "A tool for populating Celestial from files"
+  :url "https://github.com/celestial-ops/celestial-populate"
+  :license {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+  
+  :dependencies [
+     [slingshot "0.10.3" ]
+     [http-kit "2.1.16"]
+     [com.taoensso/timbre "2.6.3"]
+     [org.clojure/clojure "1.6.0"]
+  ]
+)
