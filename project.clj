10c3359166cae3b394cfc73e6655659261bcfde9
ronen <narkisr@gmail.com>
2015-04-15 19:46:25 +0300

    more deps and project renamed

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
-(defproject celestial-populate "0.1.0-SNAPSHOT"
-  :description "A tool for populating Celestial from files"
+(defproject populous "0.1.0-SNAPSHOT"
+  :description "A tool for populating Celestial from edn files"
   :url "https://github.com/celestial-ops/celestial-populate"
   :license {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   
   :dependencies [
+     [cheshire "5.4.0"]
      [slingshot "0.10.3" ]
+     [org.clojure/core.incubator "0.1.3"]
      [http-kit "2.1.16"]
      [com.taoensso/timbre "2.6.3"]
      [org.clojure/clojure "1.6.0"]
